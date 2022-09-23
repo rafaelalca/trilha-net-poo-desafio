420e0fdf8e6e261ae52be9cee2ee68a66d86676d
Rafael Alcântara <r4f4elr@gmail.com>
2022-09-23 01:47:04 -0300

    Desafio POO
    Foi aplicada as alterações pedidas e fazendo com que o sistema possa executar.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizar os testes com as classes Nokia e Iphone           
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1",imei: "1111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero:"4987", modelo:"Modelo 2", imei:"2222222", memoria: 128 );
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
